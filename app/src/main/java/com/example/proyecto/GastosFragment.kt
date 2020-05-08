0a3f3ea8279773956b44e482d0eb72569afde7de
CFlores7 <00163518@uca.edu.sv>
2020-05-08 17:23:18 -0600

    Creacion Fragment Gastos,Nuevo Ingreso

---
+++ app/src/main/java/com/example/proyecto/GastosFragment.kt
@@ -0,0 +1,60 @@
+package com.example.proyecto
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import com.example.proyecto.databinding.FragmentGastosBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class GastosFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentGastosBinding>(inflater,
+            R.layout.fragment_gastos, container, false)
+
+        centerTitle()
+
+        return binding.root
+    }
+    //Centrar texto en ActionBar
+    private fun centerTitle() {
+        val textViews = ArrayList<View>()
+        activity?.window?.decorView?.findViewsWithText(textViews, activity?.title, View.FIND_VIEWS_WITH_TEXT)
+        if (textViews.size > 0) {
+            var appCompatTextView: AppCompatTextView? = null
+            if (textViews.size == 1)
+                appCompatTextView = textViews[0] as AppCompatTextView
+            else {
+                for (v in textViews) {
+                    if (v.parent is Toolbar) {
+                        appCompatTextView = v as AppCompatTextView
+                        break
+                    }
+                }
+            }
+            if (appCompatTextView != null) {
+                val params = appCompatTextView.layoutParams
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT
+                appCompatTextView.layoutParams = params
+                appCompatTextView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            }
+        }
+    }
+    //Setting Title
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity as AppCompatActivity).supportActionBar?.title = "GASTOS"
+    }
+}
---
+++ app/src/main/java/com/example/proyecto/NuevoIngresoFragment.kt
@@ -0,0 +1,60 @@
+package com.example.proyecto
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import com.example.proyecto.databinding.FragmentNuevoIngresoBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class NuevoIngresoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentNuevoIngresoBinding>(inflater,
+            R.layout.fragment_nuevo_ingreso, container, false)
+
+        centerTitle()
+
+        return binding.root
+    }
+    //Centrar texto en ActionBar
+    private fun centerTitle() {
+        val textViews = ArrayList<View>()
+        activity?.window?.decorView?.findViewsWithText(textViews, activity?.title, View.FIND_VIEWS_WITH_TEXT)
+        if (textViews.size > 0) {
+            var appCompatTextView: AppCompatTextView? = null
+            if (textViews.size == 1)
+                appCompatTextView = textViews[0] as AppCompatTextView
+            else {
+                for (v in textViews) {
+                    if (v.parent is Toolbar) {
+                        appCompatTextView = v as AppCompatTextView
+                        break
+                    }
+                }
+            }
+            if (appCompatTextView != null) {
+                val params = appCompatTextView.layoutParams
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT
+                appCompatTextView.layoutParams = params
+                appCompatTextView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            }
+        }
+    }
+    //Setting Title
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity as AppCompatActivity).supportActionBar?.title = "NUEVO INGRESO"
+    }
+}
