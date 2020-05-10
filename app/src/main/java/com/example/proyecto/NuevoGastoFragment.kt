0daea956e205b66144a2b1c33d58c45a12d3a41d
CFlores7 <00163518@uca.edu.sv>
2020-05-09 19:35:53 -0600

    Fix DatePicker

---
+++ app/src/main/java/com/example/proyecto/NuevoGastoFragment.kt
@@ -0,0 +1,95 @@
+package com.example.proyecto
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import com.example.proyecto.databinding.FragmentNuevoGastoBinding
+import java.util.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class NuevoGastoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentNuevoGastoBinding>(inflater,
+            R.layout.fragment_nuevo_gasto, container, false)
+
+        centerTitle()
+
+        Toast.makeText(this.activity,
+            "El gasto no puede registrarse si es anterior a 3 dias.",
+            Toast.LENGTH_LONG).show()
+
+        binding.btFecha.setOnClickListener {
+            chooseDate(binding.etFecha)
+        }
+
+        return binding.root
+    }
+    //Dialogo para elegir fecha
+    private fun chooseDate(etFecha: EditText){
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val minDay = 3*24*60*60*1000
+
+        val dpd = DatePickerDialog(
+            activity as AppCompatActivity,
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+
+                // Display Selected date in textbox
+                etFecha.setText("" + dayOfMonth + "/" + monthOfYear + "/" + year)
+            },
+            year,
+            month,
+            day
+        )
+        dpd.datePicker.minDate = (System.currentTimeMillis() - minDay)
+        dpd.show()
+    }
+    //Setting Title
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity as AppCompatActivity).supportActionBar?.title = "NUEVO GASTO"
+    }
+    //Centrar texto en ActionBar
+    private fun centerTitle() {
+        val textViews = ArrayList<View>()
+        activity?.window?.decorView?.findViewsWithText(textViews, activity?.title, View.FIND_VIEWS_WITH_TEXT)
+        if (textViews.size > 0) {
+            var appCompatTextView: AppCompatTextView? = null
+            if (textViews.size == 1)
+                appCompatTextView = textViews[0] as AppCompatTextView
+            else {
+                for (v in textViews) {
+                    if (v.parent is Toolbar) {
+                        appCompatTextView = v as AppCompatTextView
+                        break
+                    }
+                }
+            }
+            if (appCompatTextView != null) {
+                val params = appCompatTextView.layoutParams
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT
+                appCompatTextView.layoutParams = params
+                appCompatTextView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/proyecto/SignUpFragment.kt
+++ app/src/main/java/com/example/proyecto/SignUpFragment.kt
@@ -1,14 +1,23 @@
 package com.example.proyecto
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.example.proyecto.databinding.FragmentSignUpBinding
+import kotlinx.android.synthetic.main.fragment_sign_up.*
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -26,8 +35,6 @@ class SignUpFragment : Fragment() {
 
         //Pais Seleccionado Default -> El Salvador
         binding.spPais.setSelection(58)
-
-
 
         return binding.root
     }
