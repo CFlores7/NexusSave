37b518a82ee5e3e68c3e4897941f8f7287215189
CFlores7 <00163518@uca.edu.sv>
2020-07-05 21:23:30 -0600

    Stats Fragment finished

--- app/src/main/java/com/example/proyecto/EstadisticasFragment.kt
+++ app/src/main/java/com/example/proyecto/EstadisticasFragment.kt
@@ -1,8 +1,8 @@
 package com.example.proyecto
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,18 +17,18 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
 import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import java.text.DecimalFormat
 
 
 /**
  * A simple [Fragment] subclass.
  */
 class EstadisticasFragment : Fragment() {
-    //private var yData = arrayOf(25F, 50F, 19F, 6F)
-    //private var xData = arrayOf("Luz","Ingresos","Internet","Agua")
     private val userID = FirebaseAuth.getInstance().currentUser!!.uid
     private val db = FirebaseFirestore.getInstance()
     private val collectionRef = db.collection("users")
@@ -36,6 +36,7 @@ class EstadisticasFragment : Fragment() {
     private val gastosRef = collectionRef.document(userID).collection("gastos")
     private val pagosRef = collectionRef.document(userID).collection("pagos")
     private lateinit var pieChart: PieChart
+    private var mContext: Context? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -46,6 +47,7 @@ class EstadisticasFragment : Fragment() {
         pieChart = binding.PieChart
 
         pieChart.description.text = "Manejo de su dinero"
+        pieChart.description.textSize = 16F
         pieChart.isRotationEnabled = true
         pieChart.holeRadius = 15F
         pieChart.setTransparentCircleAlpha(0)
@@ -114,28 +116,66 @@ class EstadisticasFragment : Fragment() {
                         pieDataSet.valueTextSize = 12F
 
                         //Add color to dataset
+                        val MY_COLORS = intArrayOf(
+                            Color.rgb(0, 126, 214),
+                            Color.rgb(131, 153, 235),
+                            Color.rgb(142, 108, 239),
+                            Color.rgb(156, 70, 208),
+                            Color.rgb(199, 88, 208),
+                            Color.rgb(224, 30, 132),
+                            Color.rgb(255, 0, 0),
+                            Color.rgb(255, 115, 0),
+                            Color.rgb(255, 175, 0),
+                            Color.rgb(255, 236, 0),
+                            Color.rgb(213, 243, 11),
+                            Color.rgb(82, 215, 38),
+                            Color.rgb(27, 170, 47),
+                            Color.rgb(45, 203, 117),
+                            Color.rgb(38, 215, 174),
+                            Color.rgb(124, 221, 221),
+                            Color.rgb(95, 183, 212),
+                            Color.rgb(151, 217, 255)
+                        )
                         val colors = ArrayList<Int>()
-                        colors.add(Color.BLUE)
-                        colors.add(Color.CYAN)
-                        colors.add(Color.LTGRAY)
-                        colors.add(Color.RED)
 
-                        pieDataSet.colors = colors
+                        for (c in MY_COLORS) colors.add(c)
+
+                        pieDataSet.setColors(colors)
+
+                        //Show selected value in Pie Chart
+                        pieChart.setOnChartValueSelectedListener(object : OnChartValueSelectedListener {
+                            override fun onValueSelected(e: Entry?, h: Highlight?) {
+                                if (e == null) return
+
+                                Toast.makeText(
+                                    mContext,
+                                    "Concepto: "+ xData[h?.x!!.toInt()] + "\n Monto: $" + e.y,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            override fun onNothingSelected() {}
+                        })
 
                         //Add legend to chart
                         var legend = pieChart.legend
                         legend.form = Legend.LegendForm.CIRCLE
 
                         //Create Pie Data Object
-                        val pieData = PieData(pieDataSet)
-                        pieChart.data = pieData
+                        val data = PieData(pieDataSet)
+
+                        data.setValueFormatter(PercentFormatter(pieChart))
+
+                        pieChart.data = data
+                        pieChart.setUsePercentValues(true)
+                        pieChart.highlightValue(null)
                         pieChart.invalidate()
                     }
                 }
             }
         }
-
-
     }
-
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
 }
