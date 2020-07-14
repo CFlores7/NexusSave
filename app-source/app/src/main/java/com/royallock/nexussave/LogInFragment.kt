f5d8e9a4baa10a72ff1533869a773077390adee8
CFlores7 <00163518@uca.edu.sv>
2020-07-14 15:05:44 -0600

    Last commit

--- app-source/app/src/main/java/com/royallock/nexussave/LogInFragment.kt
+++ app-source/app/src/main/java/com/royallock/nexussave/LogInFragment.kt
@@ -1,5 +1,22 @@
 package com.royallock.nexussave
 
+/*
+    This file is part of Nexus$ave.
+
+    Nexus$ave is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Nexus$ave is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Nexus$ave.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
---
+++ app-source/app/src/main/java/com/royallock/nexussave/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.royallock.nexussave
+
+/*
+    This file is part of Nexus$ave.
+
+    Nexus$ave is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Nexus$ave is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Nexus$ave.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
